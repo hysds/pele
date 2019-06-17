112a91f4bdfbdbdb1ecb3cf502ff861fa616b950
Gerald Manipon <pymonger@gmail.com>
2019-06-17 18:10:13 +0000

    sync version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pele',
-    version='0.1.1',
+    version='0.0.1',
     long_description='REST API for HySDS Datasets',
     packages=find_packages(),
     include_package_data=True,
