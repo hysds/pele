1b49d61587d8f64264ee88851b930759941d4611
dustinklo <dustinlo.pcc@gmail.com>
2021-04-12 13:16:02 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pele',
-    version='1.0.1',
+    version='1.0.2',
     long_description='REST API for HySDS Datasets',
     packages=find_packages(),
     include_package_data=True,
