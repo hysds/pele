ba3a8d207f0b4bfec3944dbe0a1a458688a3e08e
dustinlo <dustin.k.lo@jpl.nasa.gov>
2022-07-26 13:47:01 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pele',
-    version='1.1.4',
+    version='1.1.5',
     long_description='REST API for HySDS Datasets',
     packages=find_packages(),
     include_package_data=True,
