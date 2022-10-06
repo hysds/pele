aa6cac67b7a0863054a9392c218b0f87ccf2866d
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2022-10-06 13:55:06 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pele',
-    version='1.1.5',
+    version='1.1.6',
     long_description='REST API for HySDS Datasets',
     packages=find_packages(),
     include_package_data=True,
