3062227255c76baadf2ecd7fcfb783eaf7af0f37
Michael Cayanan <42812746+mcayanan@users.noreply.github.com>
2026-04-23 10:10:26 -0700

    HC-606: Updates to support OpenSearch 3.x (#39)
    * HC-606: Migrate to OpenSearch 3.x
    
    * Bump versions
    
    ---------
    
    Co-authored-by: Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pele',
-    version='1.4.0',
+    version='1.5.0',
     long_description='REST API for HySDS Datasets',
     packages=find_packages(),
     include_package_data=True,
@@ -26,7 +26,7 @@ setup(
         'Flask-WTF>=0.15.1',
         "elasticsearch>=7.0.0,<7.14.0",
         'elasticsearch-dsl>=7.0.0,<7.4.0',
-        'opensearch-py>=2.3.0,<3.0.0',
+        'opensearch-py>=2.3.0',
         'shapely>=1.5.15',
         'PyJWT==1.7.1',
         'WTForms>=2.0.0',
